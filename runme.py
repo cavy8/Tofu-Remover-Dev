7f8a4e11410d46901ccabb9693cb6a321cdf936a
cavy8 <cavy081499@gmail.com>
2025-09-02 18:42:16 -0700

    Set up basic structure

--- runme.py
+++ runme.py
@@ -5,19 +5,34 @@ import argparse
 parser = argparse.ArgumentParser(prog="runme.py")
 parser.add_argument("-v", "--verbose", action="store_true", default=False)
 parser.add_argument("--mode", choices=["DSD", "DDR"], default="DDR")
+parser.add_argument("--level", choices=["full", "DDR", "DSD"], default="full")
 args = vars(parser.parse_args())
 
 mode = args["mode"]
 verbose = args["verbose"]
+level = args["level"]
 
-def pull_anyascii_table(filename):
-    url = "https://raw.githubusercontent.com/anyascii/anyascii/refs/heads/master/table.tsv"
-    
-    urlretrieve(url, filename)
 
 def vprint(string):
     if verbose:
         print(string)
 
+def pull_anyascii_table(filename):
+    url = "https://raw.githubusercontent.com/anyascii/anyascii/refs/heads/master/table.tsv"
+    
+    vprint("Retrieving table from AnyASCII repo...")
+    urlretrieve(url, filename)
+
+def processDSD():
+    vprint("Processing in DSD mode...")
+
+def processDDR(ddr_level):
+    vprint(f"Processing in DDR mode. Level: {ddr_level}")
 
 pull_anyascii_table("table.tsv")
+
+if mode == "DSD":
+    processDSD()
+
+if mode == "DDR":
+    processDDR(level)
